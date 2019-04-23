d1976d9716d19eafebb68fa47b7080e6d722f32e
Rob Majuri <robmajuri@gmail.com>
2019-04-23 16:06:25 -0400

    successfully displaying current temperature and warnings for dangerous conditions

--- client/main.js
+++ client/main.js
@@ -1,20 +1,32 @@
-// window.addEventListener('load', () => {
-//     fetch(`/weather-api`)
-//     .then(response => {
-//       console.log('HERE IS THE FIRST REPONSE IN MAIN', response)
-//       return response.json()
-//     })
-//     .then(data => {
-//       console.log(data)
-//     })
-// })
 $(document).ready(function() {
-  fetch(`/weather-api`)
-    .then(response => {
-      console.log('HERE IS THE FIRST REPONSE IN MAIN', response)
-      return response.json()
-    })
-    .then(data => {
-      console.log(data)
-    })
+
+  let api = `/weather-api`
+
+  $.getJSON(api, function(res) {
+
+    const main = res.main
+    const temp = parseInt(main.temp, 10)
+    const weatherDescription = res.weather[0].description
+    const windSpeed = parseInt(res.wind.windSpeed, 10)
+    let warning
+
+
+    $('#degreesFahrenheit').html(temp);
+    $('#weatherDescription').html(weatherDescription);
+
+    if (temp > 98) {
+      warning = 'Warning: temperatures exceeding 98°F! Stay cool!'
+    } else if (temp < 32) {
+      warning = 'Warning: temperatures below 32°F! Roads may be icy!'
+    } else if (windSpeed > 35) {
+      warning = 'Warning: wind speeds exceeding 35 mph!'
+    }
+
+    if (warning) {
+      $('#weatherWarning').html(warning);
+    }
+
+
+    console.log(res)
+  })
 })
