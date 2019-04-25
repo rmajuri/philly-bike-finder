53bae44d5bfd7171000bc0d4399a44759b0dadac
Rob Majuri <robmajuri@gmail.com>
2019-04-25 14:00:57 -0400

    best practice adjustments. Deleted unneeded file and cleaned up littered variables

--- client/main.js
+++ client/main.js
@@ -1,33 +1,41 @@
 $(document).ready(function() {
 
-  //reference to a route on the server of this application
-  let api = `/weather-api`
-
-  $.getJSON(api, function(res) {
+  $.getJSON(`/weather-api`, function(res) {
 
     //main contains current temp, humidity, temp_min, and temp_max
-    const main = res.main
-    const temp = parseInt(main.temp, 10)
-    const weatherDescription = res.weather[0].description
-    const windSpeed = parseInt(res.wind.windSpeed, 10)
-    let warning
-
-
-    $('#degreesFahrenheit').html(temp);
-    $('#weatherDescription').html(weatherDescription);
+    const dataResponse = res.main
+    const temperatureData = parseInt(dataResponse.temp, 10)
+    const weatherDescriptionData = res.weather[0].description
+    const windSpeedData = parseInt(res.wind.windSpeed, 10)
 
     //Conditions that determine whether to show a weather warning
-    if (temp > 98) {
-      warning = 'Warning: temperatures exceeding 98°F! Stay cool!'
-    } else if (temp < 32) {
-      warning = 'Warning: temperatures below 32°F! Roads may be icy!'
-    } else if (windSpeed > 35) {
-      warning = 'Warning: wind speeds exceeding 35 mph!'
+    function determineWeatherWarning(temperature, wind) {
+      let warningMessage
+      let weatherWarning = $('#weatherWarning')
+
+      if (temperature > 98) {
+        warningMessage = 'Warning: temperatures exceeding 98°F! Stay cool!'
+      } else if (temperature < 32) {
+        warningMessage = 'Warning: temperatures below 32°F! Roads may be icy!'
+      } else if (wind > 35) {
+        warningMessage = 'Warning: wind speeds exceeding 35 mph!'
+      }
+
+      if (warningMessage) {
+        weatherWarning.html(warningMessage);
+      }
     }
 
-    if (warning) {
-      $('#weatherWarning').html(warning);
+    function setWeatherInfo(temperature, description) {
+      const temperatureComponent = $('#degreesFahrenheit')
+      const weatherDescriptionComponent = $('#weatherDescription')
+      temperatureComponent.html(temperature);
+      weatherDescriptionComponent.html(description);
     }
+
+    determineWeatherWarning(temperatureData, windSpeedData)
+    setWeatherInfo(temperatureData, weatherDescriptionData)
+
     console.log(res)
   })
 })
