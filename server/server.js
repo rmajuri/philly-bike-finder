a95939c4bdf3302494355e9410ab66a4528ef1c7
Rob Majuri <robmajuri@gmail.com>
2019-04-26 23:46:26 -0400

    attempt deploy again

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./app');
 const server = http.createServer(app);
-const PORT = 3000;
+const PORT = 8080;
 
 const init = () => {
   server.listen(PORT, () => {
